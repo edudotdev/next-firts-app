d865a7d7df9c58f351b072fd66b9be479d7a8ad4
Eduardo Guadalupe Robles Ley <edu@Eduardos-Air-M1.lan>
2021-08-23 00:50:42 -0700

    first commit

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const About = () => {
+  return <h1>Hola desde About</h1>
+}
+
+export default About
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Index = () => {
+  return <h1>Hola desde Index</h1>
+}
+
+export default Index
